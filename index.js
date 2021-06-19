e4c0ecf152c2650b97fa5ee3f2b2d42d068a179d
Meagan Campbell <meagancampbell@users.noreply.github.com>
2021-06-19 20:31:06 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+//Create an array that can hold 4 items name practiceFile.
+//array
+practiceFile = [273.15];
+//variables
+let num = 42;
+let greeting = 'hello';
+let string = false, -4.6, '87';
+
+practiceFile.push(num);
+console.log(practiceFile);
+
+practiceFile.push(greeting);
+console.log(practiceFile);
+
+practiceFile.push(false);
+console.log(practiceFile);
+
+
+
+//Use the bracket notation method to add "42" and "hello" to the array. Add these new items one at a time.  Print the array after each step to confirm the changes.
+
+
+
+//Use a SetValue to add the items "false", and "-4.6", and “87” to the array.  Print the array to confirm the changes.
