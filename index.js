0fa1ef36fa14c4b4b4eac22f6b318c506a9478a8
Meagan Campbell <meagancampbell@users.noreply.github.com>
2021-06-20 21:45:36 +0000

    MC -you dont have to put them in variables in order to push them

--- index.js
+++ index.js
@@ -3,19 +3,19 @@
 practiceFile = [273.15];
 //variables
 let num = 42;
-let greeting = 'hello';
-let string = false, -4.6, '87';
+// let greeting = 'hello';
+// let string = false, -4.6, '87';
 
-practiceFile.push(num);
+practiceFile.push(42);
 console.log(practiceFile);
 
-practiceFile.push(greeting);
+practiceFile.push('hello');
 console.log(practiceFile);
 
-practiceFile.push(false);
+practiceFile.push(false, -4.6, '87');
 console.log(practiceFile);
 
-
+// so you can't push a string into an array that has a negative number//
 
 //Use the bracket notation method to add "42" and "hello" to the array. Add these new items one at a time.  Print the array after each step to confirm the changes.
 
